e289cec78a4993abeba6c7970ef82d5bb96f3da6
Julian Sanchez <julian.sanchez@morningstar.com>
2021-02-11 17:13:33 -0500

    modifying api host

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5c4b542de7934a44a46215eb1f0c424-1900235489.us-east-2.elb.amazonaws.com/api/v0'
 };
 
 /*
